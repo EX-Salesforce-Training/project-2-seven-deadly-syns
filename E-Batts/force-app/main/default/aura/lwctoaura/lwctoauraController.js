277eb2d445c5944303bc27163446958342b3ecc0
ashbeenow123 <94003927+ashbeenow123@users.noreply.github.com>
2022-01-09 08:33:16 -0800

    daybefore

--- E-Batts/force-app/main/default/aura/lwctoaura/lwctoauraController.js
+++ E-Batts/force-app/main/default/aura/lwctoaura/lwctoauraController.js
@@ -2,24 +2,25 @@
 	handleQty : function(component, event, helper) {
         const qty= event.getParam('qtyVar');
         component.set('v.qtyVar1', qty);
-        component.set('v.rfqLine.Qty__c', qty);
-        
+        /*component.set('v.rfqLine.Qty__c',qty);
+        console.log('v.rfqLine.Qty__c'); 
+        console.log(v.qtyVar1);*/
 	},
     handleDate : function(component, event, helper) {
         const datevar = event.getParam('dateVar');
         component.set('v.dateVar1', datevar);
-        component.set('v.rfq.Deadline__c',datevar);
+       /* component.set('v.rfq.Deadline__c',datevar);*/
         
 	},
     handleProd : function(component, event, helper) {
         const prodvar = event.getParam('prodVar');
         component.set('v.prodVar1', prodvar);
-        component.set('v.rfqLine.Product__c',prodvar);
+      /*  component.set('v.rfqLine.Product__c',prodvar);*/
 	},
     handleProdName : function(component, event, helper) {
         const pnamevar = event.getParam('pnameVar');
         component.set('v.pnameVar1', pnamevar);
-        component.set('v.rfqLine.ProductName__c',pnamevar);
+       /* component.set('v.rfqLine.ProductName__c',pnamevar);*/
 	},
     handleUser : function(component, event, helper) {
        const userId=(event.getParam('userVar'));
@@ -31,13 +32,13 @@
             console.log(state);
             if (state === "SUCCESS") {
                 let result= response.getReturnValue();
-                let aname=result[0].Name;
                 console.log(response.getReturnValue());
                 console.log(result[0].Name);
                 console.log(result[0].Id);
                 component.set("v.acctnameVar1", result[0].Name);
+                component.set('v.acctVar1',result[0].Id);
+                /*component.set("v.rfq.AccountName__c", aname);
                 component.set("v.rfq.AccountId__c", result[0].Id);
-                component.set("v.rfq.AccountName__c", aname);
                 let rfqName= result[0].Name +"_RFQ";
                 let rfqLineName=result[0].Name + "_RFQLine";
                 component.set("v.rfq.Name",rfqName);
@@ -48,7 +49,7 @@
                 let theDisplayItems=component.get("v.rfqLines");
                 let displayItem=component.get("v.rfqLine");
                 theDisplayItems.push(displayItem);
-                component.set("v.rfqLines",theDisplayItems);
+                component.set("v.rfqLines",theDisplayItems);*/
             }
             else {
                 console.log("Failed with state: " + state);
@@ -57,6 +58,34 @@
 	});  $A.enqueueAction(action);
         
       
-	}
+	},
+    
+    handleClick : function(component, event, helper) {
+    let qty = component.get('v.qtyVar1');
+    let aname=component.get('v.acctnameVar1');
+    let dated = component.get('v.dateVar1');
+    let prodid= component.get('v.prodVar1');
+    let prodname= component.get('v.pnameVar1');
+    let acId=component.get('v.acctVar1');
+    let action=component.get('c.createRecord');
+    
+    action.setParams({'qty':qty,
+    'aname':aname,
+    'dated':dated,
+    'prodid':prodid,
+    'prodname':prodname,
+    'acId':acId});
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            console.log(state);
+        });
+    $A.enqueueAction(action);  
+        component.set('v.qtyVar1','');
+        component.set('v.acctnameVar1','');
+        component.set('v.dateVar1','');
+        component.set('v.prodVar1','');
+        component.set('v.pnameVar1','');
+        component.set('v.acctVar1','');
+}
     
 })
