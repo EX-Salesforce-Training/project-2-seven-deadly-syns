bbdc3c3303f44a4e9700a1e584d9a9ccc68b6098
Bao Nguyen <baonguyen299@gmail.com>
2022-01-08 23:48:08 -0500

    first commit

---
+++ force-app/main/default/aura/nav/navController.js
@@ -0,0 +1,46 @@
+({
+    myAction : function(component, event, helper) {
+
+    }
+})
+
+
+
+/*({
+    onclick : function(component, event, helper) {
+    let currentContent = 'Lithium-Ion Batteries';
+    let lithiumProducts = false;
+    let nickelProducts = false;
+    let leadProducts = false;
+    let ultraProducts = false;
+    
+        let evnt = event.detail.name;
+
+        this.currentContent = evnt;
+
+        if(evnt == 'Lithium-Ion Batteries'){
+            this.lithiumProducts = true;
+        } else { 
+            this.lithiumProducts = false;
+        }
+
+        if(evnt == 'Nickel-Metal Batteries'){
+            this.nickelProducts = true;
+        } else { 
+            this.nickelProducts = false;
+        }
+
+        if(evnt == 'Lead-Acid Batteries'){
+            this.leadProducts = true;
+        } else { 
+            this.leadProducts = false;
+        }
+
+        if(evnt == 'Ultracapacitors'){
+            this.ultraProducts = true;
+        } else { 
+            this.ultraProducts = false;
+        }
+    }
+    }
+)*/
---
+++ force-app/main/default/lwc/menu/menu.js
@@ -0,0 +1,62 @@
+
+import { LightningElement, wire, track } from 'lwc';
+
+import getLiProducts from '@salesforce/apex/apexController.getLiProducts';
+import getNiProducts from '@salesforce/apex/apexController.getNiProducts';
+import getPbProducts from '@salesforce/apex/apexController.getPbProducts';
+import getUProducts from '@salesforce/apex/apexController.getUProducts';
+
+
+export default class Menu extends LightningElement {
+    @wire (getLiProducts) liProducts;
+    @wire (getNiProducts) niProducts;
+    @wire (getPbProducts) pbProducts;
+    @wire (getUProducts) uProducts;
+
+    @track currentContent = 'Lithium-Ion Batteries';
+    @track lithiumProducts = false;
+    @track nickelProducts = false;
+    @track leadProducts = false;
+    @track ultraProducts = false;
+
+    //display content based on the current event/which tab is being clicked on
+    handleNavigation(e){
+        let selected = e.detail.name;
+
+        this.currentContent = selected;
+
+        if(selected == 'Lithium-Ion Batteries'){
+            this.lithiumProducts = true;
+        } else { 
+            this.lithiumProducts = false;
+        }
+
+        if(selected == 'Nickel-Metal Batteries'){
+            this.nickelProducts = true;
+        } else { 
+            this.nickelProducts = false;
+        }
+
+        if(selected == 'Lead-Acid Batteries'){
+            this.leadProducts = true;
+        } else { 
+            this.leadProducts = false;
+        }
+
+        try{
+            if(selected == 'Ultracapacitors'){
+            this.ultraProducts = true;
+        } else { 
+            this.ultraProducts = false;
+        } 
+    } catch (e) {
+        console.log(e);
+        
+    }
+        
+    }
+
+}
+
+
+
